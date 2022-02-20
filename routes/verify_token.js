4abf6cfd66ea3a463b10a9de87540c75d469c081
sooryasrajan <soorya.s27@gmail.com>
2022-02-20 22:47:22 +0530

    Modified verify token to return user information

--- routes/verify_token.js
+++ routes/verify_token.js
@@ -3,8 +3,26 @@ const router = express.Router();
 const verifyAuth = require("../middleware/verify_auth");
 
 module.exports = router.post("/", verifyAuth(["admin", "super_admin", "user"], true), (req, res) => {
-    res.status(200).send({
-        message: "Success"
+    const { _id } = req.body;
+    //get the email, name and password from database using _id
+    User.findById(_id, function (err, user) {
+        if (!err) {
+            //send the user data
+            return res.status(200).send({
+                message: "Token is valid",
+                data: {
+                    name: user.name,
+                    role: user.role,
+                    email: user.email,
+                    phoneNumber: user.phoneNumber,
+                }
+            })
+        } else {
+            return res.status(500).send({
+                message: "Failed to fetch user",
+                error: err
+            })
+        }
     });
-}
-);
+});
+
